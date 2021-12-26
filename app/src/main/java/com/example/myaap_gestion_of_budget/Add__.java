1e5b3c7e399eabd9906ceea1b91406319d9b15bb
aitmbareksaid <saidaitmbarek581@gmail.com>
2021-12-26 04:14:49 +0100

    git branch Said_ait_mbarek_branch

--- app/src/main/java/com/example/myaap_gestion_of_budget/Add__.java
+++ app/src/main/java/com/example/myaap_gestion_of_budget/Add__.java
@@ -3,20 +3,68 @@ package com.example.myaap_gestion_of_budget;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Add__ extends AppCompatActivity {
+
+    EditText Groupname;
+    EditText groupdescription;
+    EditText grouptype;
+    DatabaseReference mDatabase= FirebaseDatabase.getInstance().getReferenceFromUrl("https://myaapgestionofbudget-default-rtdb.firebaseio.com/");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+        mDatabase = FirebaseDatabase.getInstance().getReference("Groups");
+
+        Button buttonSave = findViewById(R.id.button);
 
 
+
+        Groupname = findViewById(R.id.editTextTextPersonName4);
+        groupdescription = findViewById(R.id.editTextTextPersonName5);
+        grouptype=findViewById(R.id.autoCompleteTextView) ;
+        String Id = mDatabase.push().getKey();
+        
+
+
+        buttonSave.setOnClickListener(v -> {
+
+
+            String groupn = Groupname.getText().toString();
+            String des =  groupdescription.getText().toString();
+            String gtype = grouptype.getText().toString();
+
+            if (groupn.isEmpty() || des.isEmpty() || gtype.isEmpty()) {
+                Toast.makeText(Add__.this, "Please fill the fields ", Toast.LENGTH_SHORT).show();
+            } else {
+
+                mDatabase.child(Id).child("Id").setValue(Id);
+                mDatabase.child(Id).child("Group Name").setValue(groupn);
+                mDatabase.child(Id).child("Description").setValue(des);
+                mDatabase.child(Id).child("Group Type").setValue(gtype);
+
+
+                Toast.makeText(Add__.this, "Group created !", Toast.LENGTH_SHORT).show();
+
+                // clear fields
+                Groupname.getText().clear();
+                groupdescription.getText().clear();
+                grouptype.getText().clear();
+
+            }
+        });
         //initiaize and assign variables
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
@@ -56,4 +104,6 @@ public class Add__ extends AppCompatActivity {
 
         });
     }
+
+
 }
