0a2ff8bacec475e856fa7e6e6554c3ff6af62919
abdellatif <abdellatifbouzalim@gmail.com>
2021-12-27 20:49:01 +0100

    add_Budget

--- app/src/main/java/com/example/myaap_gestion_of_budget/GroupActions.java
+++ app/src/main/java/com/example/myaap_gestion_of_budget/GroupActions.java
@@ -8,13 +8,23 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class GroupActions extends AppCompatActivity {
+    private Button addBudgetB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group_actions);
-
+        this.addBudgetB = findViewById(R.id.toaddbudget);
         Button showdashboard = findViewById(R.id.showdashboard);
+
+
+        addBudgetB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent otherActivity =  new Intent(getApplicationContext(),  addBudget.class);
+                startActivity(otherActivity);
+
+            }});
 
         showdashboard.setOnClickListener(new View.OnClickListener() {
             @Override
