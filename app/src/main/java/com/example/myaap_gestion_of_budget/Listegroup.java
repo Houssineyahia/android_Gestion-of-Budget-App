922eb512ac5ec21c283f494c2042595e265151d8
younesbouchbouk <younesbouchbouk.py@gmail.com>
2021-12-29 17:13:29 +0100

    Revert "pr etat"
    This reverts commit 409cf458f0f9e7c6c5c93299973fd7b06cbc6c37.

--- app/src/main/java/com/example/myaap_gestion_of_budget/Listegroup.java
+++ app/src/main/java/com/example/myaap_gestion_of_budget/Listegroup.java
@@ -6,7 +6,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
@@ -17,11 +16,6 @@ import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -29,8 +23,6 @@ public class Listegroup extends AppCompatActivity {
     private ArrayList<GroupClass> liste;
     private ArrayAdapter adapter;
     private static final int RQ_CODE_EDITION = 1;
-    DatabaseReference databasereference= FirebaseDatabase.getInstance().getReferenceFromUrl("https://myaapgestionofbudget-default-rtdb.firebaseio.com/");
-
 
     public ArrayList<GroupClass> initData(){
         Resources res = getResources();
@@ -39,14 +31,10 @@ public class Listegroup extends AppCompatActivity {
         final String[] ids = res.getStringArray(R.array.Groupids);
         ArrayList<GroupClass> liste2;
         liste2 = new ArrayList<>();
-
-
-
-
-
-        /*for (int i=0; i<libelles.length; ++i) {
+        for (int i=0; i<libelles.length; ++i) {
             liste2.add(new GroupClass(libelles[i], Creatore_name[i] , ids[i]));
-        }*/
+        }
+
         return liste2;
 
     }
@@ -61,56 +49,10 @@ public class Listegroup extends AppCompatActivity {
         setContentView(R.layout.activity_listegroup);
 
         liste = new ArrayList<>();
-        //ArrayList<GroupClass> SecandL = new  ArrayList<>();
-       // liste = initData();
-        Log.i("choufhna" , "ok");
-
-
+        liste = initData();
         ListView lv = (ListView) findViewById(R.id.mygroups);
-
-
-
-        databasereference.child("User_enrolments").child("younssbouch").addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-
-                for (DataSnapshot groupid : snapshot.getChildren()) {
-                    databasereference.child("Groups").child(groupid.getKey()).addListenerForSingleValueEvent(new ValueEventListener() {
-                        @Override
-                        public void onDataChange(@NonNull DataSnapshot snapshot) {
-
-                            Log.i("88",String.valueOf(snapshot.child("Group Name").getValue()));
-                            Log.i("89",String.valueOf(snapshot.child("Group Admin").getValue()));
-                            Log.i("90",String.valueOf(snapshot.getKey().toString()));
-
-
-                            liste.add(new GroupClass(String.valueOf(snapshot.child("Group Name").getValue()) , String.valueOf(snapshot.child("Group Admin").getValue()) , snapshot.getKey().toString()));
-
-                        }
-
-                        @Override
-                        public void onCancelled(@NonNull DatabaseError error) {
-
-                        }
-                    });
-
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
-
-
-        if(liste.size() != 0){
-            adapter = new GroupListeAdapter(this,liste);
-
-            lv.setAdapter(adapter);
-
-        }
-
+        adapter = new GroupListeAdapter(this,liste);
+        lv.setAdapter(adapter);
         //+ button Click Event
         FloatingActionButton addGroup = findViewById(R.id.floatingActionButton);
 
@@ -118,8 +60,6 @@ public class Listegroup extends AppCompatActivity {
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        Toast.makeText(getApplicationContext(), "You have chosen the pen: " + " " + String.valueOf(liste.size()), Toast.LENGTH_LONG).show();
-
                         startActivity(new Intent(getApplicationContext(),Add__.class));
 
                     }
@@ -141,6 +81,7 @@ public class Listegroup extends AppCompatActivity {
                     }
                 }
         );
+
 
 
 
